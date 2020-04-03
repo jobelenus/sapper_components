8eba46b5cc5cd4ff313285a44b90c63f964530e3
John Obelenus <jobelenus@gmail.com>
2020-04-03 07:23:55 -0400

    Adding utilities and turning the index page into a dashboard-looking page.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,45 @@
-<style>
-	img {
-		width: 100%;
-		max-width: 400px;
-	}
+<style type="text/scss">
+  .switcher.second {
+    /* example of overriding layout for a container */
+    --measure: 340px;
+    --s1: .5em;
+
+    .card.rounded {
+      /* example of overriding a radius */
+      --radius: 30px;
+    }
+  }
+  .switcher {
+    margin-top: 1.5em;
+  }
+  .card {
+    float: left;
+    height: 5em;
+    border: 1px solid var(--light-border);
+    background: var(--light-muted-color);
+  }
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Dashboard Example</title>
 </svelte:head>
 
-<div class="flow">
-  <h1>Great success!</h1>
+<h1>Dashboard Example</h1>
 
-  <figure>
-    <img alt='Borat' src='great-success.png'>
-    <figcaption>HIGH FIVE!</figcaption>
-  </figure>
+<div class="switcher first">
+  <div>
+    <div class="card rounded shadow hover"></div>
+    <div class="card rounded shadow depth2 hover"></div>
+    <div class="card rounded shadow depth3 hover"></div>
+    <div class="card rounded shadow depth4"></div>
+  </div>
+</div>
 
-  <p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<div class="switcher second three-wide">
+  <div>
+    <div class="card rounded shadow hover"></div>
+    <div class="card shadow depth2 hover"></div>
+    <div class="card shadow depth3 hover"></div>
+    <div class="card shadow depth4"></div>
+  </div>
 </div>
