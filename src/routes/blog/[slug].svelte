80321e22da403e4e9d89708f7f20a58a1a6c16cc
John Obelenus <jobelenus@gmail.com>
2020-04-05 12:02:46 -0400

    Fix styles in blog, I had changed the html

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -26,12 +26,12 @@
 		so we have to use the :global(...) modifier to target
 		all elements inside .content
 	*/
-	.content :global(h2) {
+	article :global(h2) {
 		font-size: 1.4em;
 		font-weight: 500;
 	}
 
-	.content :global(pre) {
+	article :global(pre) {
 		background-color: #f9f9f9;
 		box-shadow: inset 1px 1px 5px rgba(0,0,0,0.05);
 		padding: 0.5em;
@@ -39,16 +39,16 @@
 		overflow-x: auto;
 	}
 
-	.content :global(pre) :global(code) {
+	article :global(pre) :global(code) {
 		background-color: transparent;
 		padding: 0;
 	}
 
-	.content :global(ul) {
+	article :global(ul) {
 		line-height: 1.5;
 	}
 
-	.content :global(li) {
+	article :global(li) {
 		margin: 0 0 0.5em 0;
   }
 </style>
