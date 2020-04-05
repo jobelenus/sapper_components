6a75f230e67daf588b67add71480a8dedf21a124
John Obelenus <jobelenus@gmail.com>
2020-04-05 13:26:46 -0400

    Adding note to use whatever you want.

--- src/routes/login/_layout.svelte
+++ src/routes/login/_layout.svelte
@@ -25,10 +25,15 @@
     margin-bottom: auto;
     height: 30vh;  /* added to  every-layout cover rules, for the animation to not change the height of container */
   }
+
+  .cover p {
+    text-align: center;
+    font-size: .7em;
+  }
 </style>
 
 <div class="cover">
   <div></div>
   <div class="login"><slot></slot></div>
-  <div></div>
+  <div><p>Note: this is not a "real" login, use any data you want, even for the auth code.</p></div>
 </div>
