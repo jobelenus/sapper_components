fdb6d16ab8ec042afdd86d7f3dd3d3a27dfe05e1
John Obelenus <jobelenus@gmail.com>
2020-04-06 16:47:17 -0400

    Switching from polka to express

--- src/server.js
+++ src/server.js
@@ -1,17 +1,20 @@
 import sirv from 'sirv';
-import polka from 'polka';
+import express from 'express';
 import compression from 'compression';
+import cookieParser from 'cookie-parser'
 import * as sapper from '@sapper/server';
+import session from 'express-session'
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+const app = express()
+app.use(compression({ threshold: 0 }))
+app.use(sirv('static', { dev }))
+app.use(sapper.middleware())
+app.use(cookieParser())
+app.use(session())
+
+app.listen(PORT, err => {
+  if (err) console.log('error', err);
+});
