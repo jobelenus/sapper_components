18470b375e4aa24447410434cbebb69bca9f252f
John Obelenus <jobelenus@gmail.com>
2020-04-19 09:41:01 -0400

    Moving toasts up to the top

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,7 @@
+<script>
+import {toast, clear} from '../components/ToastStore.js';
+</script>
+
 <style type="text/scss">
   .sized_components {
     margin-top: 1.5em;
@@ -33,15 +37,24 @@
 <h1>Dashboard Example</h1>
 
 <section class="sized_components first">
-  <div class="card rounded shadow hover"></div>
-  <div class="card rounded shadow depth2 hover"></div>
-  <div class="card rounded shadow depth3 hover"></div>
+  <div class="card rounded shadow hover">
+    <button on:click={() => {toast({label: 'Good', message: 'Thumbs up', success: true})}}>Good toast</button>
+  </div>
+  <div class="card rounded shadow depth2 hover">
+    <button on:click={() => {toast({label: 'Bad', message: 'What are you doing', error: true})}}>Bad toast</button>
+  </div>
+  <div class="card rounded shadow depth3 hover">
+    <button on:click={() => {clear()}}>Clear toasts</button>
+  </div>
   <div class="card rounded shadow depth4"></div>
 </section>
 
 <section class="sized_components second">
-  <div class="card rounded shadow hover"></div>
-  <div class="card shadow depth2 hover"></div>
-  <div class="card shadow depth3 hover"></div>
+  <div class="card rounded shadow hover">
+  </div>
+  <div class="card shadow depth2 hover">
+  </div>
+  <div class="card shadow depth3 hover">
+  </div>
   <div class="card shadow depth4"></div>
 </section>
